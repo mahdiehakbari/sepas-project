054948cb05d43ef15df7dc695ed7f6a979ee9760
mahdieh <mahdiehakbari16@gmail.com>
2025-10-20 13:19:20 +0330

    work on menu

--- src/features/mainPage/components/CreditPlans/CreditPlans.tsx
+++ src/features/mainPage/components/CreditPlans/CreditPlans.tsx
@@ -1,35 +1,62 @@
 'use client';
 import Image from 'next/image';
 import { useTranslation } from 'react-i18next';
-
 import Link from 'next/link';
 import { CreditItems } from './constants/CreditItems';
+
 export const CreditPlans = () => {
   const { t } = useTranslation();
 
   return (
-    <div className='mx-auto max-w-4xl mb-12  px-6 md:px-0'>
+    <div className='mx-auto max-w-4xl mb-12 px-6 md:px-0'>
       <h2 className='text-[16px] font-[700] text-black mb-4'>
         {t('home:credit_plans')}
       </h2>
 
-      <div className='flex justify-between flex-wrap'>
-        {CreditItems.map((item) => (
-          <Link key={item.alt} href={item.href} rel='noopener noreferrer'>
-            <div className='text-center bg-secondary px-4 pt-[11px] pb-4 mb-2 md:mb-0 rounded-2xl transition-all duration-300 hover:shadow-lg'>
-              <Image
-                src={item.src}
-                alt={item.alt}
-                width={128}
-                height={128}
-                className='cursor-pointer hover:opacity-80 mx-auto'
-              />
-              <p className='text-black font-[500] text-[14px] mt-2'>
-                {t(item.titleKey)}
-              </p>
+      <div className='flex justify-between flex-wrap gap-4'>
+        {CreditItems.map((item, index) => {
+          const isActive = index === 0;
+
+          return (
+            <div key={item.alt} className='relative w-[128px]'>
+              {isActive ? (
+                <Link href={item.href} rel='noopener noreferrer'>
+                  <div className='text-center bg-secondary px-4 pt-[11px] pb-4 rounded-2xl transition-all duration-300 hover:shadow-lg cursor-pointer'>
+                    <Image
+                      src={item.src}
+                      alt={item.alt}
+                      width={128}
+                      height={128}
+                      className='mx-auto hover:opacity-80'
+                    />
+                    <p className='text-black font-[500] text-[14px] mt-2'>
+                      {t(item.titleKey)}
+                    </p>
+                  </div>
+                </Link>
+              ) : (
+                <div className='text-center bg-secondary px-4 pt-[11px] pb-4 rounded-2xl relative'>
+                  <Image
+                    src={item.src}
+                    alt={item.alt}
+                    width={128}
+                    height={128}
+                    className='mx-auto'
+                  />
+                  <p className='text-black font-[500] text-[14px] mt-2'>
+                    {t(item.titleKey)}
+                  </p>
+
+                  <div className='absolute inset-0 flex items-center justify-center bg-black/60 bg-opacity opacity-0 hover:opacity-100 transition-opacity rounded-2xl'>
+                    <span className='text-white font-bold text-[14px]'>
+                      {t('home:coming_soon')}
+                    </span>
+                  </div>
+                </div>
+              )}
             </div>
-          </Link>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
