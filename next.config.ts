f709fdc44bc65cf0d5ec0f68343d0e7108036cd3
mahdieh <mahdiehakbari16@gmail.com>
2025-11-04 18:00:46 +0330

    work on payment

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,8 @@
-// const nextConfig = {
-//   async rewrites() {
-//     return [
-//       {
-//         source: '/api/:path*',
-//         destination: 'https://dentalitapi.sepasholding.com/api/:path*',
-//       },
-//     ];
-//   },
-//   images: {
-//     domains: ['trustseal.enamad.ir'],
-//   },
-// };
-
-// module.exports = nextConfig;
-// next.config.js
 const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/external/:path*', // فقط مسیرهای external
+        source: '/api/:path*',
         destination: 'https://dentalitapi.sepasholding.com/api/:path*',
       },
     ];
